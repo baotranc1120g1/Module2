2b97a4bd0a856061eeb0d9a7686c15f0e8cb25d3
baotran <75417690+baotranc1120g1@users.noreply.github.com>
2021-01-11 11:23:15 +0700

    Add files via upload

--- Introduction to Java/Tinh_so_ngay_thang/src/tinhdaymonth.java
+++ Introduction to Java/Tinh_so_ngay_thang/src/tinhdaymonth.java
@@ -6,45 +6,31 @@ public class tinhdaymonth {
 
         System.out.print("Which month that you want to count days? ");
         int month = scanner.nextInt();
+        String daysInMonth;
         switch (month) {
+            case 2:
+                daysInMonth = "28 or 29";
+                break;
             case 1:
-                System.out.print("The month '" + month + "' has 31 days!");
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                daysInMonth = "31";
                 break;
-            case 2 :
-                System.out.print("The month '2' has 28 or 29 days!");
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = "30";
                 break;
-            case 3 :
-                System.out.print("The month '" + month + "' has 31 days!");
-                break;
-            case 4 :
-                System.out.print("The month '" + month + "' has 30 days!");
-                break;
-            case 5 :
-                System.out.print("The month '" + month + "' has 31 days!");
-                break;
-            case 6 :
-                System.out.print("The month '" + month + "' has 30 days!");
-                break;
-            case 7 :
-                System.out.print("The month '" + month + "' has 31 days!");
-                break;
-            case 8 :
-                System.out.print("The month '" + month + "' has 31 days!");
-                break;
-            case 9 :
-                System.out.print("The month '" + month + "' has 30 days!");
-                break;
-            case 10 :
-                System.out.print("The month '" + month + "' has 31 days!");
-                break;
-            case 11 :
-                System.out.print("The month '" + month + "' has 30 days!");
-                break;
-            case 12 :
-                System.out.print("The month '" + month + "' has 31 days!");
-                break;
-
-
+            default:
+                daysInMonth = "";
+        }
+        if (daysInMonth != "") System.out.printf("The month '%d' has %s days!", month, daysInMonth);
+        else System.out.print("Invalid input!");
         }
     }
-}
+
