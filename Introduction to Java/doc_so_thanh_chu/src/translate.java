7d85b7293df4024910849cc10c24ad1b9928270e
baotran <75417690+baotranc1120g1@users.noreply.github.com>
2021-01-13 23:51:17 +0700

    Add files via upload

---
+++ Introduction to Java/doc_so_thanh_chu/src/translate.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class translate {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so can doi: ");
+        int so = scanner.nextInt();
+
+
+        if (so <= 10) {
+
+        }
+
+    }
+}
