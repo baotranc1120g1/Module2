c8328d5f90c71a345fdd872d7d2d9e0a26881dc5
baotran <75417690+baotranc1120g1@users.noreply.github.com>
2021-01-15 10:50:29 +0700

    Add files via upload

---
+++ KeThua/Point2DandPoint3D/src/Point3D.java
@@ -0,0 +1,36 @@
+public class Point3D extends Point2D {
+    private float z;
+
+    public Point3D (float x,float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public Point3D() {
+
+    }
+
+    public void getZ(float z) {
+       this.z = z;
+    }
+
+    public float setZ() {
+        return this.z;
+    }
+
+    public void setXYZ (float x,float y,float z) {
+        super.setX(x);
+        super.setY(y);
+        this.z = z;
+
+    }
+
+    public float[] getXYZ() {
+        return new float[] {super.getX(),super.getY(),this.z};
+    }
+
+    @Override
+    public String toString() {
+        return "(" + super.getX() + "," + super.getY() + "," + this.z +")";
+    }
+}
