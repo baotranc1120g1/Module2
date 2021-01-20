ce31cbb0ba28c3c9d62088498ed7be1561e003a9
baotran <75417690+baotranc1120g1@users.noreply.github.com>
2021-01-20 21:47:17 +0700

    Update MyLinkedListTest.java

--- DSA_List/ClassLinkedList/src/MyLinkedListTest.java
+++ DSA_List/ClassLinkedList/src/MyLinkedListTest.java
@@ -3,12 +3,12 @@ public class MyLinkedListTest {
     public static void main(String[] args) {
         System.out.println("/=/=/=/= TESTING /=/=/=/=");
         MyLinkedList ll = new MyLinkedList(9);
-        ll.addFirst(11);
-        ll.addFirst(12);
-        ll.addFirst(13);
+        ll.addFirst(10);
+        ll.addFirst(18);
+        ll.addFirst(23);
 
-        ll.add(4,9);
-        ll.add(4,8);
+        ll.add(6,9);
+        ll.add(6,8);
         ll.printList();
 
     }
